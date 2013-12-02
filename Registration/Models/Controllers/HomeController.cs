0aa7dd605eb1021e41abeb28cf4391c4f2737581
Fatum94 <o_L_e_j@mail.ru>
2013-12-02 18:59:39 +0200

    Update HomeController.cs

--- Registration/Models/Controllers/HomeController.cs
+++ Registration/Models/Controllers/HomeController.cs
@@ -118,7 +118,7 @@ namespace System.Web.Security
         {
             var database = new Database();
             var arr = database.Compressor.ToArray();
-            var path = Path.GetFullPath("history.csv");
+            var path = Server.MapPath("~/App_Data/uploads/history.csv");
             var sw = new StreamWriter(path);
             var j = 0;          
 
