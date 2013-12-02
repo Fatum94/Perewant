417b7833fb1b4cb2250d9946e093bb42e634455d
Fatum94 <o_L_e_j@mail.ru>
2013-12-02 18:10:39 +0200

    Update HomeController.cs

--- Registration/Models/Controllers/HomeController.cs
+++ Registration/Models/Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace System.Web.Security
             if (FileUpload != null && FileUpload.ContentLength > 0)
             {
                 string fileName = Path.GetFileName(FileUpload.FileName);
-                string path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                string path = HttpContext.Current.Server.MapPath(@"~App_Data/" + fileName);
                 try
                 {
                     FileUpload.SaveAs(path);
