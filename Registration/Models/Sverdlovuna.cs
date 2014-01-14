1129855837a11ccf1c0f776174602ade2def9fb6
fatum94 <olehbr29@gmail.com>
2014-01-14 20:49:03 +0200

    tata

---
+++ Registration/Models/Sverdlovuna.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Registration.Models
+{
+    public class Sverdlovuna
+    {
+        public string ChooseRodo { get; set; }
+        public bool BushSverd { get; set; }
+        public int CountWorked { get; set; }
+        public int CountInclude { get; set; }
+    }
+}
---
+++ Registration/Models/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Registration.Models
+{
+    public class ViewModel
+    {
+        public Kompressor First { get; set; }
+        public Sverdlovuna Second{ get; set; }
+
+    }
+}
